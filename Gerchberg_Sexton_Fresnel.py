1a64ad56dfaf606f4cad82916d794c4b3df6acf0
Timur Minikhanov <minikhanovtz@yandex.ru>
2022-05-04 16:15:08 +0300

    add ability to place image in an arbitrary position in GS_Fresnel

--- Gerchberg_Sexton_Fresnel.py
+++ Gerchberg_Sexton_Fresnel.py
@@ -3,24 +3,24 @@ import cv2
 import matplotlib.pyplot as plt
 import cmath
 import time 
-class GS_Frenel_synthesis(object):
+class GS_Fresnel_synthesis(object):
     
-    def __init__(self, 
+    def __init__(
+                 self, 
                  holo_pixel_size, 
                  distance, wavelength, 
                  holo_size, error_dif, 
                  iter_limit, holo_type, 
                  dynamic_range,
-                  restored_img_size = None
-                  ):
+        ):
 
         self.dynamic_range = dynamic_range
         self.holo_type = holo_type
         self.name = 'Gerchberg_Sexton'
-        self._first_frenel_factor = None
-        self._second_frenel_factor = None
-        self._first_inv_frenel_factor = None
-        self._second_inv_frenel_factor = None
+        self._first_fresnel_factor = None
+        self._second_fresnel_factor = None
+        self._first_inv_fresnel_factor = None
+        self._second_inv_fresnel_factor = None
         self.holo_pixel_size = holo_pixel_size
         self.distance = distance
         self.wavelength = wavelength
@@ -30,40 +30,45 @@ class GS_Frenel_synthesis(object):
         self.error_lists = []
         self.img_pixel_size = self.wavelength * self.distance / (self.holo_size * self.holo_pixel_size)
         self.scale = self.img_pixel_size / self.holo_pixel_size
-        
-        if restored_img_size  is None:
-            self.restored_img_size = int(self.holo_size/2)
-        elif self.holo_size % restored_img_size != 0 and self.holo_type == 'phase':
-            print("Incorrect input of the new image size, the original size must be divided by it without remainder")
-            exit()
-        else: self.restored_img_size = restored_img_size
             
                 
-    def reshape_img_for_syn(self, input_matrix):
-        
+    def reshape_img_for_syn(self, input_matrix, position, restored_img_size, reshaped_img_position_coord_h_w ):
+
+        if restored_img_size  is None:
+            self.restored_img_size = int(self.holo_size/2)
+        else:
+             self.restored_img_size = restored_img_size
+
         new_img = np.zeros((self.holo_size, self.holo_size))
         reshape_img = cv2.resize(input_matrix, (self.restored_img_size,self.restored_img_size))
-        if self.holo_type == "phase":
+
+        if position == "centre":
             index = int((self.holo_size - self.restored_img_size) / 2)
             new_img[index:index + self.restored_img_size, index:index + self.restored_img_size] = reshape_img
-        elif self.holo_type == "amplitude":
+        elif position == "free":
             new_img[
-                self.reshaped_img_coord_h:self.reshaped_img_coord_h + self.restored_img_size, 
-                self.reshaped_img_coord_w:self.reshaped_img_coord_w + self.restored_img_size,
+                reshaped_img_position_coord_h_w[0]:reshaped_img_position_coord_h_w [0]+ self.restored_img_size, 
+                reshaped_img_position_coord_h_w[1]:reshaped_img_position_coord_h_w[1] + self.restored_img_size,
                 ] = reshape_img
+        else:
+            print("Incorre input, the argument 'position' must be equal center/free, but not ", position)
+            
         self.reshape_img = reshape_img
         self.new_img = new_img
         return new_img
 
-    def informative_zone (self, input_matrix):
-        if self.holo_type == "phase":
+    def informative_zone (self,input_matrix, position, reshaped_img_position_coord_h_w):
+
+        if position == "centre":
             index = int((self.holo_size - self.restored_img_size) / 2)
             res  = input_matrix[index:index + self.restored_img_size, index:index + self.restored_img_size]
-        elif self.holo_type == "amplitude":
+        elif position == "free":
             res = input_matrix[
-                self.reshaped_img_coord_h:self.reshaped_img_coord_h + self.restored_img_size, 
-                self.reshaped_img_coord_w:self.reshaped_img_coord_w + self.restored_img_size,
+                reshaped_img_position_coord_h_w[0]:reshaped_img_position_coord_h_w[0] + self.restored_img_size, 
+                reshaped_img_position_coord_h_w[1]:reshaped_img_position_coord_h_w[1] + self.restored_img_size,
                 ]
+        else:
+            print("Incorre input, the argument 'position' must be equal center/free, but not ", position)
         return res
         
     # function for translating an argument from a range [0 2*pi) to  (-pi,pi]
@@ -87,9 +92,9 @@ class GS_Frenel_synthesis(object):
  
 
     @property
-    def first_frenel_factor(self):
-        if self._first_frenel_factor is None:
-            self._first_frenel_factor = (
+    def first_fresnel_factor(self):
+        if self._first_fresnel_factor is None:
+            self._first_fresnel_factor = (
                 np.exp(
                     np.array([
                         [
@@ -100,12 +105,12 @@ class GS_Frenel_synthesis(object):
                     ])
                 )
             ) 
-        return self._first_frenel_factor
+        return self._first_fresnel_factor
 
     @property
-    def second_frenel_factor(self):
-        if self._second_frenel_factor is None:
-            self._second_frenel_factor = (
+    def second_fresnel_factor(self):
+        if self._second_fresnel_factor is None:
+            self._second_fresnel_factor = (
                 np.exp(
                     np.array([
                         [
@@ -116,12 +121,12 @@ class GS_Frenel_synthesis(object):
                     ])
                 )
             )
-        return self._second_frenel_factor
+        return self._second_fresnel_factor
       
     @property
-    def first_inv_frenel_factor(self):
-        if self._first_inv_frenel_factor is None:
-            self._first_inv_frenel_factor = np.exp(
+    def first_inv_fresnel_factor(self):
+        if self._first_inv_fresnel_factor is None:
+            self._first_inv_fresnel_factor = np.exp(
                 np.array([
                     [
                         -1j * np.pi * ((i- int(self.holo_size / 2))**2 + (j - int(self.holo_size / 2))**2)/(self.scale * self.holo_size)
@@ -130,12 +135,12 @@ class GS_Frenel_synthesis(object):
                     for i in range(self.holo_size)
                 ])
             )
-        return self._first_inv_frenel_factor
+        return self._first_inv_fresnel_factor
 
     @property
-    def second_inv_frenel_factor(self):
-        if self._second_inv_frenel_factor is None:
-            self._second_inv_frenel_factor = np.exp(
+    def second_inv_fresnel_factor(self):
+        if self._second_inv_fresnel_factor is None:
+            self._second_inv_fresnel_factor = np.exp(
                 np.array([
                     [
                         -1j * np.pi * self.scale *  ((i - int(self.holo_size / 2))**2 + (j - int(self.holo_size / 2))**2) / (self.holo_size)
@@ -144,24 +149,24 @@ class GS_Frenel_synthesis(object):
                     for i in range(self.holo_size)
                 ])
             )
-        return self._second_inv_frenel_factor
+        return self._second_inv_fresnel_factor
 
     def zero_to_two_pi_range(self, phase): 
         return (phase + 2 * np.pi) * (phase < 0) + (phase) * (phase >= 0)
 
-    def frenel_transform(self,input_matrix):
-        fourier = np.fft.ifftshift(np.fft.fft2(np.fft.fftshift(input_matrix * self.first_frenel_factor)))
-        return  self.second_frenel_factor * fourier
+    def fresnel_transform(self,input_matrix):
+        fourier = np.fft.ifftshift(np.fft.fft2(np.fft.fftshift(input_matrix * self.first_fresnel_factor)))
+        return  self.second_fresnel_factor * fourier
     
-    def inverse_frenel_transform(self,input_matrix):
-        inv_fourier = np.fft.ifftshift(np.fft.ifft2(np.fft.ifftshift(input_matrix * self.first_inv_frenel_factor)))
-        return self.second_inv_frenel_factor * inv_fourier
+    def inverse_fresnel_transform(self,input_matrix):
+        inv_fourier = np.fft.ifftshift(np.fft.ifft2(np.fft.ifftshift(input_matrix * self.first_inv_fresnel_factor)))
+        return self.second_inv_fresnel_factor * inv_fourier
     
     
     def initial_approx(self):
         return np.exp(1j * np.random.rand(self.holo_size, self.holo_size))
     
-    def prepare_for_frenel (self, input_matrix):
+    def prepare_for_fresnel (self, input_matrix):
         return np.exp(1j * input_matrix * 2 * np.pi / 256)
 
     def calc_error(self, input_matrix, real_img):
@@ -172,49 +177,63 @@ class GS_Frenel_synthesis(object):
         ab = np.sum(input_matrix*real_img)
         return np.sqrt(1-(ab*ab)/(a*b))
 
-    def img_recovery(self, holo):
+    def img_recovery(self, holo, position, reshaped_img_position_coord_h_w):
         if self.holo_type == "phase":
-            rec_img = abs(self.frenel_transform(self.prepare_for_frenel(holo)))
+            rec_img = abs(self.fresnel_transform(self.prepare_for_fresnel(holo)))
         elif self.holo_type == "amplitude":
-            rec_img = abs(self.frenel_transform(holo))
+            rec_img = abs(self.fresnel_transform(holo))
+        else: 
+            print("Incorre input, the argument 'holo_type' must be equal phase/amplitude, but not ", self.holo_type)
+
         rec_img  = rec_img**2   
         self.rec_img = rec_img
         plt.imshow(rec_img, cmap = "gray")
         plt.show()
-        self.informative_img_zone = self.informative_zone(rec_img)
+        self.informative_img_zone = self.informative_zone(rec_img, position, reshaped_img_position_coord_h_w)
         plt.imshow(self.informative_img_zone, cmap = "gray")
         plt.show()
 
         
-    def __call__(self, input_matrix, control, reshaped_img_coord_h_w = (None,None)):
-        self.reshaped_img_coord_h, self.reshaped_img_coord_w = reshaped_img_coord_h_w
+    def __call__(
+                    self,
+                    input_matrix,  
+                    control=False, 
+                    position=None, 
+                    restored_img_size = None,  
+                    reshaped_img_position_coord_h_w = (None,None)
+
+                ):
+        self.reshaped_img_coord_h, self.reshaped_img_coord_w = reshaped_img_position_coord_h_w
         error_list = []
         start_time = time.time()
         holo = self.initial_approx()
-        img = self.reshape_img_for_syn(input_matrix)
-        plt.imshow(img, cmap="gray")
-        plt.show()
+        img = self.reshape_img_for_syn(input_matrix, position, restored_img_size, reshaped_img_position_coord_h_w )
+        if control:
+            plt.imshow(img, cmap="gray")
+            plt.show()
         error_dif = float('inf')
         last_error_dif = 0
         i = 0
         while error_dif > self.error_dif and i < self.iter_limit:
-            ref_img = self.frenel_transform(holo)
+            ref_img = self.fresnel_transform(holo)
             error = self.calc_error(
-                                    abs(self.informative_zone(ref_img))**2,
-                                    self.informative_zone(img)
+                                    abs(self.informative_zone(ref_img, position, reshaped_img_position_coord_h_w))**2,
+                                    self.informative_zone(img,position, reshaped_img_position_coord_h_w)
                                     )                 
             ref_img = np.sqrt(img) * np.exp(1j*np.angle(ref_img))
-            holo = self.inverse_frenel_transform(ref_img)
+            holo = self.inverse_fresnel_transform(ref_img)
+
             if self.holo_type == "phase":
                 if self.dynamic_range == "bin":
                     holo = self.zero_to_two_pi_range(np.angle(holo))
                     holo = cv2.threshold(np.uint8(holo * 255 / (2 * np.pi)), 0, 127, cv2.THRESH_OTSU)[1]
                     holo = np.exp(1j * holo * np.pi / 127 )
-                else:
+                elif self.dynamic_range == "gray":
                     holo = np.exp(1j * np.angle(holo))
-            elif self.holo_type == "amplitude": 
-                
+            elif self.holo_type == "amplitude":                
                 holo = abs(holo)
+            else: 
+                print("Incorre input, the argument 'holo_type' must be equal phase/bin, but not ", self.holo_type)   
             i += 1 
             error_list.append(error)
             print("Iteration ", i, "error(informative zone)", error) 
@@ -223,31 +242,31 @@ class GS_Frenel_synthesis(object):
         self.error_lists.append(error_list)
         holo = self.matrix_normalization(holo)
         if control:
-            self.img_recovery(holo)
+            self.img_recovery(holo,position, reshaped_img_position_coord_h_w)
         self.holo = holo
         print("--- %s seconds ---" % (time.time() - start_time))
         return holo
 
-transform = GS_Frenel_synthesis(
+transform = GS_Fresnel_synthesis(
     holo_pixel_size = 8e-6,
-    distance = 0.5,
+    distance = 0.1,
     wavelength = 532e-9,
     holo_size = 1024,
     error_dif = 1e-4,
-    holo_type = 'amplitude',
-    dynamic_range = "bin",
+    holo_type = 'phase',
+    dynamic_range = "gray",
     iter_limit = 20,
-    restored_img_size=400
+    
 )
 
 img = cv2.imread("C:\\Users\\minik\\Desktop\\lena.jpg", cv2.IMREAD_GRAYSCALE)
-holo = transform(img, reshaped_img_coord_h_w = (100,100),  control=True)
-# cv2.imwrite("C:\\Users\\minik\\Desktop\\lena_holo.bmp", holo)
+holo = transform(img, position='free', reshaped_img_position_coord_h_w = (100,100), restored_img_size=200,  control=False)
+cv2.imwrite("C:\\Users\\minik\\Desktop\\lena_holo.bmp", holo)
 #  # print(holo)
-# # # plt.imshow(holo, cmap = 'gray')
-# # plt.show()
+plt.imshow(holo, cmap = 'gray')
+plt.show()
 # holo = cv2.imread("C:\\Users\\minik\\Desktop\\lena_holo.bmp", cv2.IMREAD_GRAYSCALE)
-# plt.imshow((abs(transform.frenel_transform(holo)))**2, cmap = 'gray')
+# plt.imshow((abs(transform.fresnel_transform(holo)))**2, cmap = 'gray')
 # plt.show()
-# # plt.plot(abs(transform.frenel_transform(np.exp(1j*holo)))**2)
+# # plt.plot(abs(transform.fresnel_transform(np.exp(1j*holo)))**2)
 # # plt.show()
